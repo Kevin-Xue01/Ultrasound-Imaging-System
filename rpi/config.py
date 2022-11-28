ee7687c6eb865b034624c3c68bdfbbe3012f7196
Anni Wang <anniwang44#gmail.com>
2022-11-28 18:08:25 -0500

    new images

--- rpi/config.py
+++ rpi/config.py
@@ -3,24 +3,31 @@ import cv2
 def nothing(x):
     pass
 
-PATH = "../data/paper_1/test.png"
+name = "scissors"
+n = 4
+
+PATH = f'../data/{name}_{n}/test.png'
+NEW_PATH = f"../data/{name}_{n}/new_image.png"
 def main():
     cv2.namedWindow("Trackbar")
     cv2.resizeWindow("Trackbar", 800, 200)  # width, height
     cv2.createTrackbar(
-        "Width", "Trackbar", 20, 1000, nothing
+        "Width", "Trackbar", 630, 1000, nothing
     )
     cv2.createTrackbar(
-        "Height", "Trackbar", 20, 1000, nothing
+        "Height", "Trackbar", 400, 1000, nothing
     )
 
     img = cv2.imread(PATH)
-    width = 20
-    height = 20
+    img = cv2.bitwise_not(img)
+    img = cv2.applyColorMap(img, cv2.COLORMAP_OCEAN)
+
+    width = 630
+    height = 400
     print(img.shape)
     while True:
         if cv2.waitKey(1) == ord("q"):  # press q to terminate program
-            cv2.imwrite(PATH, cv2.resize(img, (width, height), interpolation = cv2.INTER_AREA))
+            cv2.imwrite(NEW_PATH, cv2.resize(img, (width, height), interpolation = cv2.INTER_AREA))
             break
         width = cv2.getTrackbarPos(
             "Width", "Trackbar"
