0211a711492fbafb14cdb40d8bf12711872aeefc
Yewon Kwak <goodywk07@gmail.com>
2022-11-28 15:45:29 -0500

    add config file for image resize

---
+++ rpi/config.py
@@ -0,0 +1,29 @@
+import cv2
+  
+def nothing(x):
+    pass
+
+def main():
+    cv2.namedWindow("Trackbar")
+    cv2.resizeWindow("Trackbar", 800, 200)  # width, height
+    cv2.createTrackbar(
+        "Width", "Trackbar", 10, 1000, nothing
+    )
+    cv2.createTrackbar(
+        "Height", "Trackbar", 10, 1000, nothing
+    )
+
+    img = cv2.imread("data/paper_1/test.png")
+    while True:
+        if cv2.waitKey(1) == ord("q"):  # press q to terminate program
+            break
+        width = cv2.getTrackbarPos(
+            "Width", "Trackbar"
+        )
+        height = cv2.getTrackbarPos(
+            "Height", "Trackbar"
+        )
+        cv2.imshow("Scan", cv2.resize(img, (width, height), interpolation = cv2.INTER_AREA))
+
+if __name__ == "__main__":
+    main()
