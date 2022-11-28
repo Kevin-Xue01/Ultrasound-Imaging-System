802d3a751c69bc2a2ca3e2dfc9360af4dff53bd1
kevin-xue-uwaterloo <kevinx1698@gmail.com>
2022-11-28 12:55:50 -0800

    -

--- rpi/config.py
+++ rpi/config.py
@@ -3,6 +3,7 @@ import cv2
 def nothing(x):
     pass
 
+PATH = "../data/paper_1/test.png"
 def main():
     cv2.namedWindow("Trackbar")
     cv2.resizeWindow("Trackbar", 800, 200)  # width, height
@@ -13,9 +14,13 @@ def main():
         "Height", "Trackbar", 10, 1000, nothing
     )
 
-    img = cv2.imread("data/paper_1/test.png")
+    img = cv2.imread(PATH)
+    width = 20
+    height = 20
+    print(img.shape)
     while True:
         if cv2.waitKey(1) == ord("q"):  # press q to terminate program
+            cv2.imwrite(PATH, cv2.resize(img, (width, height), interpolation = cv2.INTER_AREA))
             break
         width = cv2.getTrackbarPos(
             "Width", "Trackbar"
